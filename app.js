c6c7edf96ba2af8495e0c8aac06a163ef596af80
Giulio Zausa <mail.zausa.giulio@gmail.com>
2016-02-15 19:11:55 +0100

    Aggiunta classi

--- app.js
+++ app.js
@@ -1,44 +1,73 @@
-var JSONUrl = 'https://api.myjson.com/bins/2gfjd';
+var JSONUrl = 'https://jsonblob.com/api/jsonBlob/56c20effe4b01190df4f22a1';
 
-var settings;
-var outpElement, finalValue;
+var settings, classe;
+var outpElement, classeSelector, classeSelectorList, finalValue;
 var personId;
-var animInterval, animDelay = 1;
+var animInterval, animDelay = 1, endAnim = false;
 
 function init() {
     outpElement = document.getElementById('persona');
+    classeSelector = document.getElementById('classSel');
+    classeSelectorList = document.getElementById('classSelList');
     $.get(JSONUrl, function(data, textStatus, jqXHR){
         settings = data;
         console.log(data);
+        classe = data.currentClass;
+        enumClassi();
         updateTable();
         textAnim();
     });
 }
 
+function enumClassi() {
+    var i;
+    for (i = 0; i < settings.class.length; i++) {
+        classeSelectorList.innerHTML += "<li><a href=\"#\" onclick=\"updateClass(" + i + ")\">" + settings.class[i].id + "</a></li>";
+    }
+    classeSelector.innerHTML = settings.class[classe].id + "<span class=\"caret\"></span>";
+}
+
+function updateClass(i) {
+    classe = i;
+    classeSelector.innerHTML = settings.class[classe].id + "<span class=\"caret\"></span>";
+    updateTable();
+    settings.currentClass = classe;
+    saveJson(false);
+}
+
 function stopButton() {
-    personId = getPersonId();
-    settings.nums[personId]++;
-    saveJson();
-    finalValue = settings.classe[personId];
-    slowTextAnim();
+    if (!endAnim) {
+        personId = getPersonId();
+        settings.class[classe].nums[personId]++;
+        saveJson(true);
+        finalValue = settings.class[classe].nomi[personId];
+        slowTextAnim();
+    }
+    else {
+        document.getElementById("stopButton").innerHTML = "STOP";
+        animDelay = 1.0;
+        clearTimeout(animInterval);
+        endAnim = false;
+        textAnim();
+    }
 }
 
 function clearTable() {
     var i;
-    for (i = 0; i < settings.classe.length; i++) {
-        settings.nums[i] = 0;
+    for (i = 0; i < settings.class[classe].nomi.length; i++) {
+        settings.class[classe].nums[i] = 0;
     }
-    saveJson();
+    saveJson(false);
     updateTable();
 }
 
 function undo() {
-    settings.nums[personId]--;
-    saveJson();
+    settings.class[classe].nums[personId]--;
+    saveJson(false);
     updateTable();
 }
 
-function saveJson() {
+function saveJson(butc) {
     $.ajax({
         url: JSONUrl,
         type: "PUT",
@@ -46,7 +75,7 @@ function saveJson() {
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
-            $("#stopButton").toggleClass('btn-success btn-danger');
+            if (butc) $("#stopButton").toggleClass('btn-success btn-danger');
             console.log(data);
             console.log(textStatus);
         }
@@ -55,14 +84,14 @@ function saveJson() {
 
 function getPersonId() {
     var wsum = 0.0, i;
-    for (i = 0; i < settings.classe.length; i++) {
-        wsum += (1.0 / Math.pow(2.0, settings.nums[i])) * 500.0;
+    for (i = 0; i < settings.class[classe].nomi.length; i++) {
+        wsum += (1.0 / Math.pow(2.0, settings.class[classe].nums[i])) * 500.0;
     }
     
     var x = 1.0 + wsum * Math.random();
     var cw = 0;
-    for (i = 0; i < settings.classe.length; i++) {
-        cw += (1.0 / Math.pow(2.0, settings.nums[i])) * 500.0;
+    for (i = 0; i < settings.class[classe].nomi.length; i++) {
+        cw += (1.0 / Math.pow(2.0, settings.class[classe].nums[i])) * 500.0;
         if (x < cw) {
             return i;
         }
@@ -72,7 +101,8 @@ function getPersonId() {
 }
 
 function textAnim() {
-    animInterval = setInterval(function(){ outpElement.innerHTML = settings.classe[Math.floor(settings.classe.length * Math.random())]}, animDelay);
+    animInterval = setInterval(function(){ outpElement.innerHTML =
+        settings.class[classe].nomi[Math.floor(settings.class[classe].nomi.length * Math.random())]}, animDelay);
 }
 
 function slowTextAnim() {
@@ -81,14 +111,16 @@ function slowTextAnim() {
 }
 
 function slowCycle() {
-    if (animDelay > 1000) {
+    if (animDelay > 800) {
         outpElement.innerHTML = finalValue;
         clearTimeout(animInterval);
         $("#stopButton").toggleClass('btn-success btn-danger');
+        document.getElementById("stopButton").innerHTML = "Ricomincia";
         updateTable();
+        endAnim = true;
     }
     else {
-        outpElement.innerHTML = settings.classe[Math.floor(settings.classe.length * Math.random())];
+        outpElement.innerHTML = settings.class[classe].nomi[Math.floor(settings.class[classe].nomi.length * Math.random())];
         animDelay *= 1.1;
         setTimeout(slowCycle, animDelay);
     }
@@ -98,16 +130,16 @@ function updateTable() {
     var table = document.getElementById('numTable');
     table.innerHTML = "<thead><tr><th>#</th><th>Nome e Cognome</th><th>N° Volte Interrogato</th></tr></thead>";
     var i;
-    for (i = 0; i < settings.classe.length; i++) {
+    for (i = 0; i < settings.class[classe].nomi.length; i++) {
         var tr = document.createElement('tr');
         var td = document.createElement('td');
         td.appendChild(document.createTextNode(i + 1));
         tr.appendChild(td);
         td = document.createElement('td');
-        td.appendChild(document.createTextNode(settings.classe[i]));
+        td.appendChild(document.createTextNode(settings.class[classe].nomi[i]));
         tr.appendChild(td);
         td = document.createElement('td');
-        td.appendChild(document.createTextNode(settings.nums[i]));
+        td.appendChild(document.createTextNode(settings.class[classe].nums[i]));
         tr.appendChild(td);
         table.appendChild(tr);
     }
